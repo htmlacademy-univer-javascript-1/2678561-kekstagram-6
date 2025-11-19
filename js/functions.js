7f19b9f1c49a7789bb244b09afdb903e3c28b813
Anton Nazarov <a.nazarov3@g.nsu.ru>
2025-11-19 20:10:11 +0700

    using basic funcions

--- js/functions.js
+++ js/functions.js
@@ -6,10 +6,10 @@ function isPalindrome(str) {
   let p1 = 0;
   let p2 = str.length - 1;
   while (p1 < p2) {
-    if (str[p1] === ' ') {
+    if (str[p1] === " ") {
       p1++;
     }
-    if (str[p2] === ' ') {
+    if (str[p2] === " ") {
       p2--;
     }
     if (str[p1].toLowerCase() !== str[p2].toLowerCase()) {
@@ -23,11 +23,29 @@ function isPalindrome(str) {
 
 function getNumber(input) {
   const str = input.toString();
-  let result = '';
+  let result = "
   for (let i = 0; i < str.length; i++) {
-    if (!Number.isNaN(parseInt(str[i]))) {
+    if (!Number.isNaN(parseInt(str[i], 10))) {
       result += str[i];
     }
   }
-  return Number(result);
+  return result === "" ? NaN : Number(result);
 }
+
+console.log(checkLength("проверяемая строка", 20));      // true
+console.log(checkLength("проверяемая строка", 18));      // true
+console.log(checkLength("проверяемая строка", 10));      // false
+
+console.log(isPalindrome("топот"));                      // true
+console.log(isPalindrome("ДовОд"));                      // true
+console.log(isPalindrome("Кекс"));                       // false
+console.log(isPalindrome("Лёша на полке клопа нашёл ")); // true
+
+console.log(getNumber("2023 год"));                      // 2023
+console.log(getNumber("ECMAScript 2022"));               // 2022
+console.log(getNumber("1 кефир, 0.5 батона"));           // 105
+console.log(getNumber("агент 007"));                     // 7
+console.log(getNumber("а я томат"));                     // NaN
+console.log(getNumber(2023));                            // 2023
+console.log(getNumber(-1));                              // 1
+console.log(getNumber(1.5));                             // 15
