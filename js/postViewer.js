3742e8d0886a1e59d6df2088d513935ff658d1cc
Anton Nazarov <a.nazarov3@g.nsu.ru>
2025-12-10 00:31:37 +0700

    fix hiding elems when open post

--- js/postViewer.js
+++ js/postViewer.js
@@ -7,8 +7,10 @@ const socialCaption = bigPicture.querySelector('.social__caption');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
 const overlay = bigPicture;
 
-bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-bigPicture.querySelector('.comments-loader').classList.add('hidden');
+const socialCommentCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+
+console.log(socialCommentCount.target);
 
 const createComment = (comment) => {
   const commentElement = document.createElement('li');
@@ -45,11 +47,17 @@ const openPhoto = (photo) => {
   });
   socialComments.appendChild(commentsFragment);
 
+  socialCommentCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
 };
 
 const closePhoto = () => {
+  socialCommentCount.classList.remove('hidden');
+  commentsLoader.classList.remove('hidden');
+
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
 };
