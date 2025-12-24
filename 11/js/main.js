e16f57f37fbd74e915bb6cbc9463cdd9f48c88e6
Keks <keks@htmlacademy.ru>
2025-12-24 11:26:28 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/main.js
+++ 11/js/main.js
@@ -1,21 +1,9 @@
 import { getData } from './modules/api/api.js';
 import { writeMiniatures } from './modules/gallery/miniaturesWriter.js';
-import { openPhoto } from './modules/gallery/postViewer.js';
 import { showGalleryError } from './modules/utils/galleryError.js';
+import { setPreviewListeners } from './modules/utils/previewListenersSetter.js';
 import './modules/form/formValidator.js';
 import './modules/form/formManager.js';
-
-const setPreviewListeners = (photosArray) => {
-  const picturesContainer = document.querySelector('.pictures');
-  const pictureElements = picturesContainer.querySelectorAll('.picture');
-
-  pictureElements.forEach((pictureElement, index) => {
-    pictureElement.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      openPhoto(photosArray[index]);
-    });
-  });
-};
 
 const loadPhotos = async () => {
   let photosArray;
--- 11/js/modules/utils/galleryError.js
+++ 11/js/modules/utils/galleryError.js
@@ -3,8 +3,36 @@ const showGalleryError = (message) => {
     .querySelector('#gallery-error')
     .content
     .cloneNode(true);
-  template.querySelector('.gallery-error__message').textContent = message;
+
+  const errorElement = template.querySelector('.gallery-error');
+  const messageElement = template.querySelector('.gallery-error__message');
+  messageElement.textContent = message;
+
   document.body.appendChild(template);
+
+  function closeModal() {
+    errorElement.remove();
+    document.removeEventListener('keydown', onEscKeyDown);
+    errorElement.removeEventListener('click', onOverlayClick);
+  }
+
+  function onEscKeyDown(evt) {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      evt.stopPropagation();
+      closeModal();
+    }
+  }
+
+  function onOverlayClick(evt) {
+    if (evt.target === errorElement) {
+      closeModal();
+    }
+  }
+
+  document.addEventListener('keydown', onEscKeyDown);
+  errorElement.addEventListener('click', onOverlayClick);
+
 };
 
 export { showGalleryError };
